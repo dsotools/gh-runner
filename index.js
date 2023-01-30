ed9cc1be1495f5ad82a81e479eb9432cae076363
Jim Seiwert <jimseiwert@gmail.com>
2023-01-30 15:42:37 -0600

    testing file download

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
+const axios = require('axios');
+const FormData = require('form-data');
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const file = core.getInput('file');
+  const saveDir = core.getInput('outputPath')
   console.log(`File Path: ${file}!`);
+
+  const upload = async () => {
+    try {
+      const file = fs.createReadStream(file);
+//      const title = 'My file';
+    
+      const form = new FormData();
+  //    form.append('title', title);
+      form.append('file', file);
+    
+      const resp = await axios.post('http://api.dsotools.com/api/upload', form, {
+        headers: {
+          ...form.getHeaders(),
+        }
+      });
+    
+      if (resp.status === 200) {
+        const path = `${__dirname}/files/img.jpeg`; 
+        const filePath = fs.createWriteStream(path);
+        res.pipe(filePath);
+        filePath.on('finish',() => {
+            filePath.close();
+            console.log('Download Completed'); 
+        })
+      } 
+    } catch(err) {
+      return new Error(err.message);
+    }
+  }
+
+  upload().then(resp => console.log(resp));
+
   const time = (new Date()).toTimeString();
   core.setOutput("filePath", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log(`The event payload: ${payload}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
