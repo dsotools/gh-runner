7746d1fed29e3ea4996547cce9e36e9af5ebee4c
Jim Seiwert <jimseiwert@gmail.com>
2023-01-30 16:28:30 -0600

    testing file upload

--- index.js
+++ index.js
@@ -4,45 +4,37 @@ const fs = require('fs');
 const axios = require('axios');
 const FormData = require('form-data');
 
+const downloadURL = "https://api.dsotools.com/api/upload"
 try {
   const filePath = core.getInput('file');
   const saveDir = core.getInput('outputPath')
   console.log(`File Path: ${filePath}!`);
 
-  const upload = async () => {
-    try {
-      const file = fs.createReadStream(filePath);
-//      const title = 'My file';
-    
-      const form = new FormData();
-  //    form.append('title', title);
-      form.append('file', file);
-    
-      const resp = await axios.post('http://api.dsotools.com/api/upload', form, {
-        headers: {
-          ...form.getHeaders(),
-        }
-      });
-    
-      if (resp.status === 200) {
-        const path = `${__dirname}/files/sample.bicep`; 
-        const filePath = fs.createWriteStream(path);
-        resp.pipe(filePath);
-        filePath.on('finish',() => {
-            filePath.close();
-            console.log('Download Completed'); 
-        })
-      } 
-    } catch(err) {
-      return new Error(err.message);
-    }
-  }
-
-  upload().then(resp => console.log(resp));
+  uploadFile(filePath,saveDir + "/response.bicep")
 
   const time = (new Date()).toTimeString();
   core.setOutput("filePath", time);
 
 } catch (error) {
   core.setFailed(error.message);
+}
+
+
+
+async function uploadFile(uploadedFile, saveFile) {
+    return new Promise(async (fulfill, reject) => {
+        const uploadFile = fs.createReadStream(uploadedFile);
+        const form = new FormData();
+        form.append('file', uploadFile);
+
+
+        let stream = fs.createWriteStream(saveFile);
+        let response = await axios.post(downloadURL, form, { responseType: 'stream',   headers: {...form.getHeaders()} });
+        
+        if (response.status === 200) {
+            response.data.pipe(stream);
+            stream.on('finish', fulfill);
+            stream.on('error', reject); // presumably
+        }
+    });
 }
